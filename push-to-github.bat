b24b78dba1f7d0ed355bf8a4e7878c8d4e6e477e
YJ Sung <yjsung@example.com>
2025-09-10 18:51:59 +0900

    Remove files containing secrets

--- push-to-github.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Pushing to GitHub...
-git push https://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@github.com/yjsung-spartan/communication-collector.git master
-echo Done!
-pause
